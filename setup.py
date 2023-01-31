3710942c396bbedebd7583517b18e267a5ae9b87
hari64 <hari64boli64@gmail.com>
2023-01-31 14:17:07 +0900

    update setup

--- setup.py
+++ setup.py
@@ -4,20 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="myawsname",
-    version="0.0.1",
-    author="cm-taro",
-    author_email="cmtaro@example.com",
+    name="fastwedge",
+    version="1.0.0",
+    author="hari64",
+    author_email="hari64boli64@gmail.com",
     license='MIT',
-    description="You can receive AWS Service Name.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cm-ichida/myawsname",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    url="https://github.com/hari64boli64/fastwedge",
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
 )
