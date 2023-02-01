e98659676185af494612d29598b0a7b1c294576d
hari64 <hari64boli64@gmail.com>
2023-02-01 16:11:50 +0900

    update setuppy

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setuptools.setup(
     version="1.0.0",
     author="hari64",
     author_email="hari64boli64@gmail.com",
-    license='MIT',
+    license="MIT",
     url="https://github.com/hari64boli64/fastwedge",
+    description="fast computation of k-RDM and wedge product",
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
 )
