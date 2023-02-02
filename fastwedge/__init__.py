1847c1352cfa529f485eeae5fcce336f2729dee3
hari64 <hari64boli64@gmail.com>
2023-02-02 11:45:33 +0900

    update test

--- fastwedge/__init__.py
+++ fastwedge/__init__.py
@@ -2,7 +2,6 @@ from fastwedge.tensorVis import visTensor, vis4dTensorNest
 from fastwedge.kRDM import fast_compute_k_rdm
 from fastwedge.wedgeProduct import fast_wedge
 from fastwedge.wedgeTopM import fast_wedge_topM
-from fastwedge.test import test
 
 __all__ = [
     "visTensor",
@@ -10,5 +9,4 @@ __all__ = [
     "fast_compute_k_rdm",
     "fast_wedge",
     "fast_wedge_topM",
-    "test"
 ]
