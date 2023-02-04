e0067cc2ab7f041f7a5634fe5628e2fa9fcd7b45
hari64 <hari64boli64@gmail.com>
2023-02-05 08:23:17 +0900

    fix bug

--- fastwedge/kRDM.py
+++ fastwedge/kRDM.py
@@ -3,7 +3,7 @@ from math import factorial
 from scipy.sparse import csr_matrix, coo_matrix
 from tqdm.notebook import tqdm
 from typing import Dict
-from itertools import combinations, combinations_with_replacement
+from itertools import combinations
 from fastwedge._basis import _generate_fixed_parity_permutations,\
     _generate_parity_permutations,\
     _getIdx
@@ -21,20 +21,41 @@ def _make_jordan_wigners_mul_vec(Q: int, k: int, vec: np.ndarray,
         data = []
         row = []
         col = []
-        mask = sum(1 << (Q-1-p) for p in ps)
-        for r in range(n_hilbert-sum(1 << (Q-1-p) for p in ps)):
-            if r & mask:
-                continue
+        offset = sum(1 << (Q-1-p) for p in ps)
+        mask = (n_hilbert-1) ^ offset
+        r = n_hilbert-offset
+        while r > 0:
+            r = (r-1) & mask
             data.append(+1 if (sum(bin(r >> (Q-1-p))[2:].count('1')
-                                   for p in ps) % 2 == 1) else -1)
+                                   for p in ps) % 2 == 0) else -1)
             row.append(r)
-            col.append(r+sum(1 << (Q-1-p) for p in ps))
+            col.append(r+offset)
 
         ans = csr_matrix((data, (row, col)),
                          shape=(n_hilbert, n_hilbert)) @ vec
         jordan_wigners_mul_vec[_getIdx(Q, *ps)] = ans
-        jordan_wigners_mul_vec[_getIdx(
-            Q, *ps[::-1])] = ans*((-1)**((k*(k-1)//2) % 2))
+        jordan_wigners_mul_vec[_getIdx(Q, *ps[::-1])] =\
+            ans*((-1)**((k*(k-1)//2) % 2))
+
+    # # slow
+    # import scipy.sparse
+    # import openfermion
+    # # Create a list of raising and lowering operators for each orbital.
+    # jw_operators = []
+    # for tensor_factor in range(Q):
+    #     jw_operators.append(
+    #         openfermion.jordan_wigner_ladder_sparse(Q,
+    #                                                 tensor_factor,
+    #                                                 0).tocsr())
+    # n_hilbert = 2**Q
+    # for ps in combinations(range(Q)[::-1], k):
+    #     sparse_matrix = scipy.sparse.identity(n_hilbert,
+    #                                           dtype=complex,
+    #                                           format='csc')
+    #     for ladder_operator in ps:
+    #         sparse_matrix = sparse_matrix * jw_operators[ladder_operator]
+    #     assert np.allclose(jordan_wigners_mul_vec[_getIdx(Q, *ps)],
+    #                        sparse_matrix @ vec)
 
     return jordan_wigners_mul_vec
 
@@ -57,37 +78,55 @@ def fast_compute_k_rdm(k: int, vec: np.ndarray,
     # 要請: k <= Q でなければならない(そうでなければ全て0)
     assert 1 <= k <= Q
 
-    rdm_data = []
-    rdm_idx = []
     fixed_k = _generate_fixed_parity_permutations(k)
-
     QCk = factorial(Q)//factorial(k)//factorial(Q-k)
+    QPk = factorial(Q)//factorial(Q-k)
+    rdm_data = [None]*(QPk**2)
+    rdm_idx = [None]*(QPk**2)
 
     jordan_wigners_mul_vec = _make_jordan_wigners_mul_vec(Q, k, vec)
 
     idx_up = Q**k
+    i = 0
 
-    for ps, qs in tqdm(combinations_with_replacement(combinations(range(Q), k),
-                                                     2),
-                       total=QCk*(QCk+1)//2,
+    for ps, qs in tqdm(combinations(combinations(range(Q), k), 2),
+                       total=QCk*(QCk-1)//2,
                        disable=not verbose):
         bra = jordan_wigners_mul_vec[_getIdx(Q, *ps[::-1])]
         ket = jordan_wigners_mul_vec[_getIdx(Q, *qs)]
         val = np.dot(bra.conj(), ket)
         val_conj = val.conj()
-        # ps==qsの場合、以下は一部無駄があるが、条件分岐を挟む方が時間が掛かりそう。
+
         for perm1, parity1 in _generate_parity_permutations(ps, fixed_k):
             val_p1 = val*parity1
             val_conj_p1 = val_conj*parity1
             idx1 = _getIdx(Q, *perm1)
             for perm2, parity2 in _generate_parity_permutations(qs, fixed_k):
                 idx2 = _getIdx(Q, *perm2)
-                rdm_data.append(val_p1*parity2)
-                rdm_data.append(val_conj_p1*parity2)
-                rdm_idx.append(idx1*idx_up+idx2)
-                rdm_idx.append(idx2*idx_up+idx1)
+                rdm_idx[i] = idx1*idx_up+idx2
+                rdm_data[i] = val_p1*parity2
+                i += 1
+                rdm_idx[i] = idx2*idx_up+idx1
+                rdm_data[i] = val_conj_p1*parity2
+                i += 1
 
-    return coo_matrix((rdm_data, (rdm_idx, [0]*len(rdm_data))),
-                      shape=(Q**(2*k), 1))\
+    for ps in combinations(range(Q), k):
+        bra = jordan_wigners_mul_vec[_getIdx(Q, *ps[::-1])]
+        ket = jordan_wigners_mul_vec[_getIdx(Q, *ps)]
+        val = np.dot(bra.conj(), ket)
+        val_conj = val.conj()
+        gpp = _generate_parity_permutations(ps, fixed_k)
+        for perm1, parity1 in gpp:
+            val_p1 = val*parity1
+            val_conj_p1 = val_conj*parity1
+            idx1 = _getIdx(Q, *perm1)
+            for perm2, parity2 in gpp:
+                idx2 = _getIdx(Q, *perm2)
+                rdm_idx[i] = idx1*idx_up+idx2
+                rdm_data[i] = val_p1*parity2
+                i += 1
+
+    return coo_matrix((rdm_data, ([0]*len(rdm_data), rdm_idx)),
+                      shape=(1, Q**(2*k)), dtype=np.complex)\
         .toarray()\
         .reshape(tuple(Q for _ in range(2*k)))
